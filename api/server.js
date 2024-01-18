00833b17b8c64e09af09e5f0842a7ba561926276
Marlon Colato <mcolato2104@gmail.com>
2024-01-17 21:19:22 -0500

    j

--- api/server.js
+++ api/server.js
@@ -11,9 +11,5 @@ server.use('/', projectsRouter);
 server.get('/', (req, res) => {
     res.send('Barrd Aim');
 })
-// Configure your server here
-// Build your actions router in /api/actions/actions-router.js
-// Build your projects router in /api/projects/projects-router.js
-// Do NOT `server.listen()` inside this file!
 
 module.exports = server;
