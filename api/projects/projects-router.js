5fb4f2f69df5a9e470cff19466dd5b6f312a9f24
Marlon Colato <mcolato2104@gmail.com>
2024-01-17 21:18:37 -0500

    router is working properly

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,9 @@
-// Write your "projects" router here!
+const express = require('express');
+ 
+const router = express.Router();
+
+router.get('/right', (req,res) => {
+    res.send('There is an enemy combarrrrrrrnnt to your left!!');
+})
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { logger } = require('./projects/projects-middleware')
+const projectsRouter = require('./projects/projects-router');
+const { logger } = require('./projects/projects-middleware');
 
 const server = express();
 
 server.use(express.json())
 server.use(logger);
+server.use('/', projectsRouter);
 
 server.get('/', (req, res) => {
     res.send('Barrd Aim');
