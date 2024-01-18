e40b21aff5cc2e2e03baff2cc5675d3cd79445d4
Marlon Colato <mcolato2104@gmail.com>
2024-01-17 21:07:55 -0500

    validateProjectID MW created

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,29 @@
-// add middlewares here related to projects
+const Projects = require('./projects-model');
+
+function logger(req, res, next) {
+    // DO YOUR MAGIC
+    console.log(
+      `[${new Date().toISOString()}] ${req.method} to ${req.url}`
+    );
+  
+    next();
+  };
+
+  async function validateProjectId(req, res, next) {
+    try{
+    const project = await Projects.get(req.params.id);
+        if(!project){
+            res.status(404).json({ message: 'no project with given id' })
+        }else{
+            req.project = project;
+            next();
+        }
+    }catch(err){
+        res.status(500).json({message: 'project is bad'})
+    } 
+  }
+
+
+  module.exports = {
+    logger
+  }
--- api/server.js
+++ api/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const { logger } = require('./projects/projects-middleware')
+
 const server = express();
 
 server.use(express.json())
+server.use(logger);
 
 server.get('/', (req, res) => {
     res.send('Barrd Aim');
